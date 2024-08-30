3e364e484febdf7e3bdf473f00d114b567182aff
drddjango <drddjango@gmail.com>
2024-08-30 10:28:24 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,18 @@
 import { MetadataRoute } from 'next'
 
-export default function robots():MetadataRoute.Robots {
-    return {
-        rules: [
-            {
-                userAgent: '*',
-                allow: '/'
-            }
-        ],
-        sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
-    }
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot-Image',
+        disallow: '/*', // Disallow everything
+        allow: '/Background/hero_sm.webp', // Allow the specific image
+      },
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+  }
 }
