c769b8bd19e88e1c7230e22c972596f31bbcaca1
drddjango <drddjango@gmail.com>
2024-09-02 16:39:54 +0530

    Update ServiceImage.tsx

--- src/app/(sections)/(treatments)/ServiceImage.tsx
+++ src/app/(sections)/(treatments)/ServiceImage.tsx
@@ -3,24 +3,29 @@
 import { Skeleton } from '@/components/ui/skeleton'
 import Image from 'next/image'
 import { useState } from 'react'
+
 interface Props {
   image: string
   alt: string
 }
+
 export const ServiceImage = ({ image, alt }: Props) => {
   const [loading, setLoading] = useState(true)
+
   return (
-    <div className="w-full h-full overflow-hidden rounded-lg">
-      {loading && <Skeleton className="w-full h-[500px] bg-gray-300" />}
+    <div className="relative w-full h-full overflow-hidden rounded-lg">
+      {loading && (
+        <Skeleton className="absolute inset-0 w-full h-full bg-gray-300" />
+      )}
       <Image
         src={image}
         alt={alt}
         style={{ objectFit: 'cover' }}
-        className="w-full"
+        className={`w-full h-full transition-opacity duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
         height={900}
         width={900}
         priority={true}
-        onLoadingComplete={() => setLoading(false)}
+        onLoad={() => setLoading(false)}
       />
     </div>
   )
