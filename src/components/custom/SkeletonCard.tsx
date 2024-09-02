c3839aebe9d70cdaf6ed9c49c3e416c98f70c341
drddjango <drddjango@gmail.com>
2024-09-02 17:06:42 +0530

    Update SkeletonCard.tsx

--- src/components/custom/SkeletonCard.tsx
+++ src/components/custom/SkeletonCard.tsx
@@ -6,8 +6,8 @@ const SkeletonCard = () => {
     <div className="flex flex-col space-y-3 justify-center">
       <Skeleton className=" w-full h-[400px] bg-gray-300 mx-auto" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[50%] bg-gray-300" />
+        <Skeleton className="h-4 w-[45%] bg-gray-300" />
       </div>
     </div>
   )
