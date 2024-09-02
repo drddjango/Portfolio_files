c9cc8ae08475ca9bf7a9ebc72a098f31c6476cc6
drddjango <drddjango@gmail.com>
2024-09-02 15:55:26 +0530

    Update page.tsx for root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next'
 import {
   AboutSection,
   ContactSection,
@@ -6,6 +7,46 @@ import {
   TestimonialsSection,
   TreatmentSection,
 } from './(sections)/(home)'
+
+export const metadata: Metadata = {
+  title: 'Welcome to ILILI Dental Spa',
+  alternates: {
+    canonical: `https://www.ililidentalspa.com/`,
+  },
+  description:
+    'Ilili Dental Spa provides top-quality dental care with a team of experienced professionals. Visit us today for a healthier smile.',
+  keywords: [
+    'dental clinic',
+    'dentist',
+    'dental care',
+    'Ilili Dental Spa',
+    'oral health',
+    'dental services',
+    'dental tourism',
+    'Trivandrum',
+    'Kananmoola dentist',
+    'root canal treatment Trivandrum',
+    'teeth whitening Trivandrum',
+    'family dentist Kananmoola',
+    'cosmetic dentistry Kerala',
+    'orthodontics Trivandrum',
+    'emergency dental care Trivandrum',
+    'pediatric dentist Trivandrum',
+    'best dentist in Trivandrum',
+    'dental implants Kananmoola',
+    'dental hygiene Trivandrum',
+    'gum disease treatment Kerala',
+    'affordable dental clinic Trivandrum',
+    'dental braces Trivandrum',
+    'pain-free dentistry Kananmoola',
+    'tooth extraction Trivandrum',
+    'dental checkup Kananmoola',
+    'oral surgery Trivandrum',
+    'Ilili Dental Spa services',
+    'top dental clinic Kerala',
+    'dental consultation Trivandrum',
+  ],
+}
 
 export default function Home() {
   return (
