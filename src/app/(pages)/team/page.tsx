9cffe82b17e49d013cab30be94053551629467bc
drddjango <drddjango@gmail.com>
2024-09-02 15:01:14 +0530

    Update metadata for team page

--- src/app/(pages)/team/page.tsx
+++ src/app/(pages)/team/page.tsx
@@ -7,7 +7,21 @@ import { Metadata } from 'next'
 type Props = {}
 
 export const metadata: Metadata = {
-  title: 'The Team',
+  title: 'Meet the Team',
+  description:
+    'Get to know the skilled and friendly team of dentists and staff at Ilili Dental Spa in Kananmoola, Trivandrum. Our experienced professionals are dedicated to providing top-quality dental care to every patient.',
+  keywords: [
+    'Ilili Dental Spa',
+    'meet the team',
+    'dentists Kananmoola',
+    'dental team Trivandrum',
+    'oral health experts',
+    'dental care professionals',
+    'dentist profiles',
+    'dental staff Kerala',
+    'experienced dentists Trivandrum',
+    'best dental team Trivandrum',
+  ],
 }
 
 const Team = (props: Props) => {
